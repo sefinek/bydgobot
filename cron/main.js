b3491c67ca2fb76e6a7c53675b377e60347e72fd
Sefinek <contact@sefinek.net>
2025-07-13 14:00:27 +0200

    Migrate to `cron`

--- cron/main.js
+++ cron/main.js
@@ -1,4 +1,4 @@
-const { schedule } = require('node-cron');
+const { CronJob } = require('cron');
 
 // Scripts
 const online = require('../scripts/online.js');
@@ -14,53 +14,48 @@ const verificationCron = require('./verification.js');
 const diagnosticCron = require('./diagnostic.js');
 
 // Variables
-const settings = { scheduled: true, timezone: 'Europe/Warsaw' };
+const timezone = 'Europe/Warsaw';
 
 module.exports = client => {
 	const guild = client.guilds.cache.get(process.env.GUILD_ID);
 	const chat = guild.channels.cache.get(process.env.CH_GENERALY);
 
-
 	// Messages
-	// schedule('37 21 * * *', async () => {
+	// new CronJob('37 21 * * *', async () => {
 	// 	await papajCron.start(guild, online, chat, banners);
-	// }, settings);
+	// }, null, true, timezone);
 	//
-	// schedule('38 21 * * *', async () => {
+	// new CronJob('38 21 * * *', async () => {
 	// 	await papajCron.end(guild, online, chat, banners);
-	// }, settings);
+	// }, null, true, timezone);
 
-	// schedule('35 17 * * 0-6/2', async () => {
+	// new CronJob('35 17 * * 0-6/2', async () => {
 	// 	await wakeupCron(guild, online, chat);
-	// }, settings);
-
+	// }, null, true, timezone);
 
 	// Change banner
-	schedule('30 6 * * *', async () => {
+	new CronJob('30 6 * * *', async () => {
 		await dayCron(guild, online, chat, banners);
-	}, settings);
+	}, null, true, timezone);
 
-	schedule('30 23 * * *', async () => {
+	new CronJob('30 23 * * *', async () => {
 		await nightCron(guild, online, chat, banners);
-	}, settings);
+	}, null, true, timezone);
 
 	// Zawka pozdrawia i praktyki <: 26.05.2022 😴 27.07.2023 🐦 21.10.2023 😿
-	schedule('30 17 * * *', async () => {
+	new CronJob('30 17 * * *', async () => {
 		await afternoonCron(guild, chat, banners);
-	}, settings);
-
+	}, null, true, timezone);
 
 	// Verification
-	schedule('30 19 * * *', async () => {
+	new CronJob('30 19 * * *', async () => {
 		await verificationCron(guild);
-	}, settings);
+	}, null, true, timezone);
 
 	// Diagnostics
-	schedule('0 5 * * 0-6/3', async () => {
+	new CronJob('0 5 * * 0-6/3', async () => {
 		await diagnosticCron(client);
-	}, settings);
-
-
+	}, null, true, timezone);
 
 	// Loaded
 	console.log('CronEx » Loaded cron tasks');
